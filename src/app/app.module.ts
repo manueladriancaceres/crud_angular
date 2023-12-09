b99b44ae79d06bee9b694ac8497f48def46b962f
manueladriancaceres <adriancaceres980@gmail.com>
2023-12-08 21:48:22 -0300

    add menu router link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,32 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ClienteComponent } from './cliente/cliente.component';
 import { ClientDetComponent } from './client-det/client-det.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClienteComponent,
-    ClientDetComponent
+    ClientDetComponent,
+    AboutComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: MenuComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'app/clientes', component: ClienteComponent },
+    ])    
   ],
   providers: [
     provideClientHydration()
